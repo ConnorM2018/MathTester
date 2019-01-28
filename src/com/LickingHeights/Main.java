d164016013bb0407b28192c1d55391ddb1a71e67
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-01-28 14:09:47 -0500

    Added the firstWord and secondWord

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,15 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	int firstNumber, secondNumber;
-	int total;
+	String firstWord, secondWord;
+	firstWord = "dog";
+	secondWord = "house";
     firstNumber = 5;
     secondNumber = 6;
-    total = (firstNumber+secondNumber);
-    System.out.println("The + of firstNumber and secondNumber is "+total);
+    System.out.println("The + of firstNumber and secondNumber is "+(firstNumber+secondNumber));
     System.out.println("The - of firstNumber and secondNumber is "+(firstNumber-secondNumber));
     System.out.println("The * of firstNumber and secondNumber is "+(firstNumber*secondNumber));
     System.out.println("The / of firstNumber and secondNumber is "+(firstNumber/secondNumber));
     System.out.println("The modulus of the secondNumber by the firstNumber is "+(secondNumber%firstNumber));
-
+    System.out.println(firstWord+(firstNumber+secondNumber));
+    System.out.println(firstWord+secondWord);
+    System.out.println(firstWord+firstNumber+secondWord+secondNumber);
+    System.out.println(firstWord+(firstNumber+secondNumber)+secondWord);
     }
 }
